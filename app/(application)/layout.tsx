bf0bf7840fe54919609432563d25e8265d107c96
Egor Shramko <egorii2001@gmail.com>
2026-08-02 00:04:52 +0700

    Добавлена мета-информация на страницу в layout.tsx

--- app/(application)/layout.tsx
+++ app/(application)/layout.tsx
@@ -4,6 +4,15 @@ import { ThemeProvider } from "@mui/material/styles";
 import { ReactNode } from "react";
 import ApplicationHeader from "./components/ApplicationHeader";
 import { Divider } from "@mui/material";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Билеты в кино",
+  description: "Система бронирования билетов в кинотеатр",
+  icons: {
+    icon: '/favicon.ico'
+  }
+}
 
 export default function RootLayout({
   children
