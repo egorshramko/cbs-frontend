7d94cdc378df59dfb14ef807bb7c95e56aafdd6e
Egor Shramko <egorii2001@gmail.com>
2026-09-19 02:18:42 +0700

    Сделал разворачивание всех сеансов в кинотеатре

--- app/(application)/movie/[id]/components/CinemaSessionCard.tsx
+++ app/(application)/movie/[id]/components/CinemaSessionCard.tsx
@@ -3,13 +3,51 @@ import CinemaInformation from "./CinemaInformation";
 import CinemaSessions from "./CinemaSessions";
 import Button from "@mui/material/Button";
 import ExpandMoreOutlinedIcon from "@mui/icons-material/ExpandMoreOutlined"
+import ExpandLessOutlinedIcon from "@mui/icons-material/ExpandLessOutlined"
 import Cinema from "../lib/Cinema";
+import { useState } from "react";
+
+function ExpandButtonIcon({
+  hallSessionsSize,
+  isExpanded
+} : {
+  hallSessionsSize: number,
+  isExpanded: boolean
+}) {
+
+  if (hallSessionsSize <= 2) {
+    return;
+  }
+
+  if (isExpanded) {
+    return (
+      <ExpandLessOutlinedIcon sx={{
+        fontSize: "32px"
+      }} />
+    )
+  }
+  else {
+    return (
+      <ExpandMoreOutlinedIcon sx={{
+        fontSize: "32px"
+      }} />
+    )
+  }
+
+}
 
 export default function CinemaSessionCard({
   cinema
 } : {
   cinema: Cinema
 }) {
+
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  function handleExpandClick() {
+    setIsExpanded(!isExpanded);
+  } 
+
   return (
     <Box sx={{
       display: "flex",
@@ -24,20 +62,22 @@ export default function CinemaSessionCard({
         justifyContent: "space-between",
         width: "100%"
       }}>
-        <CinemaSessions hallSessions={ cinema.hallSessions } />
+        <CinemaSessions hallSessions={ cinema.hallSessions } isExpanded={ isExpanded } />
         <Button sx={{
           alignItems: "flex-start",
           maxWidth: "64px",
           "&:hover": {
             backgroundColor: "white"
           }
-        }} variant="text">
+        }} variant="text"
+          disabled={ cinema.hallSessions.length <= 2 }
+          onClick={ handleExpandClick }>
           <Box sx={{
             display: "block"
           }}>
-            <ExpandMoreOutlinedIcon sx={{
-              fontSize: "32px"
-            }} />
+            <ExpandButtonIcon 
+              hallSessionsSize={ cinema.hallSessions.length }
+              isExpanded={ isExpanded } />
           </Box>
         </Button>
       </Box>
